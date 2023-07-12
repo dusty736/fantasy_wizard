cc1b2363b6e75d9cd1e8d1d24f40c9a31542c381
dusty736 <burnhamdustin@gmail.com>
2023-07-11 19:25:59 -0700

    Creat starter code to pull coaching data.

--- R/supplementary_coaches.R
+++ R/supplementary_coaches.R
@@ -36,7 +36,11 @@ coach_urls <- paste0("https://www.pro-football-reference.com", coach_links)
 
 
 # Loop over the coach URLs to fetch and process the HTML content of each coach page
-for (coach_url in coach_urls) {
+coach_results_list <- list()
+coach_team_ranks_list <- list()
+coach_full_team_hist <- list()
+for (i in seq_along(coach_urls[1:5])) {
+  coach_url <- coach_urls[i]
   # Fetch the HTML content of the page
   response <- GET(coach_url)
   html_content <- content(response, "text")
@@ -45,8 +49,8 @@ for (coach_url in coach_urls) {
   table_pattern <- "<table[^>]*>.*?</table>"
   tables <- regmatches(html_content, gregexpr(table_pattern, html_content, ignore.case = TRUE))[[1]]
   
-  for (i in seq_along(tables)) {
-    table_html <- read_html(tables[[i]])
+  for (j in seq_along(tables)) {
+    table_html <- read_html(tables[[j]])
     table_df <- table_html %>%
       html_table(fill = TRUE) %>% 
       data.frame()
