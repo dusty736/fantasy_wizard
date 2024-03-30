ef081ba8237ec7c120c289bff665f8f9d1fbaac5
dusty736 <burnhamdustin@gmail.com>
2024-03-30 12:54:46 -0700

    Add code to download nextgen stats, injuries, and contracts.

--- R/download/data_setup.R
+++ R/download/data_setup.R
@@ -19,7 +19,7 @@ if (!file.exists(file.path("data", "raw"))) {
 ################################################################################
 
 # Set sesasons
-seasons <- 2000:2023
+seasons <- 2013:2023
 
 # Download data
 pbp_data <- nflfastR::load_pbp(seasons)
@@ -50,6 +50,92 @@ if (!file.exists(folder_path)) {
 
 # Save Data
 data.table::fwrite(rosters, file.path(folder_path, "rosters.csv"),
+                   row.names = FALSE)
+
+################################################################################
+# Load Depth Charts
+################################################################################
+
+dc <- progressr::with_progress(nflreadr::load_depth_charts(seasons = seasons))
+
+# Create File Path
+folder_path <- file.path("data", "raw", "depth_charts")
+
+if (!file.exists(folder_path)) {
+  dir.create(folder_path)
+}
+
+# Save Data
+data.table::fwrite(dc, file.path(folder_path, "depth_charts.csv"),
+                   row.names = FALSE)
+
+################################################################################
+# Injuries
+################################################################################
+
+injuries <- progressr::with_progress(nflreadr::load_injuries(seasons = seasons))
+
+# Create File Path
+folder_path <- file.path("data", "raw", "injuries")
+
+if (!file.exists(folder_path)) {
+  dir.create(folder_path)
+}
+
+# Save Data
+data.table::fwrite(dc, file.path(folder_path, "injuries.csv"),
+                   row.names = FALSE)
+
+
+################################################################################
+# Participation
+################################################################################
+
+participation <- progressr::with_progress(nflreadr::load_participation(seasons = 2016:2023))
+
+# Create File Path
+folder_path <- file.path("data", "raw", "formations")
+
+if (!file.exists(folder_path)) {
+  dir.create(folder_path)
+}
+
+# Save Data
+data.table::fwrite(participation, file.path(folder_path, "formations.csv"),
+                   row.names = FALSE)
+
+################################################################################
+# NGS
+################################################################################
+
+ngs <- progressr::with_progress(nflreadr::load_nextgen_stats(seasons = 2016:2023))
+
+# Create File Path
+folder_path <- file.path("data", "raw", "nextgen_stats")
+
+if (!file.exists(folder_path)) {
+  dir.create(folder_path)
+}
+
+# Save Data
+data.table::fwrite(ngs, file.path(folder_path, "nextgen_stats.csv"),
+                   row.names = FALSE)
+
+################################################################################
+# Contracts
+################################################################################
+
+contracts <- progressr::with_progress(nflreadr::load_contracts())
+
+# Create File Path
+folder_path <- file.path("data", "raw", "contracts")
+
+if (!file.exists(folder_path)) {
+  dir.create(folder_path)
+}
+
+# Save Data
+data.table::fwrite(ngs, file.path(folder_path, "contracts.csv"),
                    row.names = FALSE)
 
 ################################################################################
